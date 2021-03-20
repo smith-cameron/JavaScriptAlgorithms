27f2a52d8d6b8eb0b3092e80d8d7e636a269a60f
Cameron Smith <theauspiciouscammy@gmail.com>
2021-03-20 06:11:20 -0700

    3/20 Algos

--- ArraysToDo1.js
+++ ArraysToDo1.js
@@ -1,108 +1,97 @@
 //Push Front
-var arr = [1,2,3,4];
-var num = 6;
+var arr = [1,2,2,3,4,5,5,6];
+var num = 12;
 var indexOf = 2;
+
 function pushFront(arr, num){
     arr.push(num);
-    var temp = 0;
-    for(var i = arr.length-1; i >= 0; i--){
-        temp = arr[i-1];
-        arr[i-1] = arr[i];
-        arr[i] = temp;
-        for(var j = arr.length-2; j >= 1; j--){
-            temp = arr[j-1];
-            arr[j-1] = arr[j];
-            arr[j] = temp;
-        }
-        return arr;
+    var temp ;
+    for(var i = arr.length-1; i > 0; i--){
+        temp = arr[i];
+        arr[i] = arr[i-1];
+        arr[i-1] = temp;
     }
+return arr;
 }
 //console.log(pushFront(arr, num));
 
 //Pop Front
 function popFront(arr){
-    var temp = 0;
-    if((arr.length % 2) != 0){
-        for(var i = 0; i <= arr.length/2-1; i++){
-            console.log(arr);
-            temp = arr[i];
-            arr[i] = arr[arr.length-1];
-            arr[arr.length-1] = temp;
-            for(var j = i+1; j <= i+1; j++){
-                temp = arr[j];
-                arr[j] = arr[arr.length-1-j];
-                arr[arr.length-1-j] = temp;
-            }
-            console.log(arr);
-            arr.pop();
-            for(var i = 0; i <= arr.length/2; i++){
-                console.log(arr);
-                temp = arr[i];
-                arr[i] = arr[arr.length-1];
-                arr[arr.length-1] = temp;
-                for(var j = i+1; j <= i+1; j++){
-                    temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-                return arr;
-            }
-        }
+    var temp ;
+    for(var i = 0; i < arr.length-1; i++){
+        temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
     }
-    else if((arr.length % 2) == 0){
-        for(var i = 0; i <= arr.length/2; i++){
-            console.log(arr);
-            temp = arr[i];
-            arr[i] = arr[arr.length-1];
-            arr[arr.length-1] = temp;
-            for(var j = i+1; j <= i+1; j++){
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-            console.log(arr);
-            arr.pop();
-            for(var i = 0; i <= arr.length; i++){
-                temp = arr[i];
-                arr[i] = arr[arr.length-1];
-                arr[arr.length-1] = temp;
-                return arr;
-            }
-        }
-    }
+    arr.pop();
+    return arr;
 }
 //console.log(popFront(arr));
 
 //Insert At
 function insert(arr, num, index){
-    arr.push(num);
-    var temp = 0;
-    for(var i = arr.length-1; i >= 0; i--){
-        temp = arr[i-1];
-        arr[i-1] = arr[i];
-        arr[i] = temp;
-        console.log(arr);
-        for(var j = arr.length-2; j >= arr[index]; j--){
-            temp = arr[j-1];
-            arr[j-1] = arr[j];
-            arr[j] = temp;
+    for(var i = 0; i < arr.length-1; i++){
+        if(arr[i] == arr[index]){
+            arr[i] = num;
         }
-        return arr;
     }
+    return arr;
 }
 //console.log(insert(arr, num, indexOf));
 
 //Remove At
 function remove(arr, index){
-    console.log(arr);
-    var temp = 0;
-    for(var i = 0; i <= arr.length-1;i++){
-        temp = arr[index];
-        arr[index] = arr[index+1];
-        arr[index+1] = temp;
-        var toRemove = arr.pop();
-        console.log(arr)
-        return toRemove;
+    var temp ;
+    for(var i = index; i < arr.length-1; i++){
+        temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
     }
+    arr.pop();
+    return arr;
 }
-console.log(remove(arr, indexOf));
+//console.log(remove(arr, indexOf));
+
+//Swap Pairs
+function swap(arr){
+    var temp1 ;
+    var temp2 ;
+    for(var i = 0; i < arr.length-1; i+=2){
+        temp1 = arr[i];
+        temp2 = arr[i+1];
+        arr[i] = temp2
+        arr[i+1] = temp1;
+    }
+    return arr;
+}
+//console.log(swap(arr));
+
+//Remove Duplicates
+function duplicates(arr){
+    var index;
+    var temp;
+    for(var i = 0; i < arr.length-1; i++){
+        console.log(i);
+        if(arr[i] != arr[i+1]){
+        }
+        else if(arr[i] == arr[i+1]){
+            console.log("arr[i]: "+arr[i]);
+            console.log("arr[i+1]: "+arr[i+1]);
+            index = i;
+            console.log("var index: "+index);
+            for(var i = index; i < arr.length-1; i++){
+                temp = arr[i];
+                console.log("temp: "+temp);
+                arr[i] = arr[i+1];
+                console.log("arr[i]: "+arr[i]);
+                arr[i+1] = temp;
+                console.log("arr[i+1]: "+arr[i+1]);
+                console.log(arr);
+            }
+            console.log(i);
+        }
+        console.log(i);
+    }
+    return arr;
+}
+console.log(duplicates(arr));
