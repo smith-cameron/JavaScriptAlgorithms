b2d9839f843bcfc9c530f27d5b08f86d727e23e5
Cameron Smith <theauspiciouscammy@gmail.com>
2021-03-22 19:44:51 -0700

    another commit

--- SLLFronts.js
+++ SLLFronts.js
@@ -1,17 +1,36 @@
-class SLL{
-    constructor(){
-        this.head = null;
+class Node{
+    constructor(value){
+        this.val = value;
+        this.next = null;
     }
-    addFront(value){
-        newNode = newNode(value);
-        newNode.next = this.head;
-        this.head = newNode;
-        return this;
-    }
-    removeFront(){
 
-    }
-    frontValue(){
 
-    }
+    // addFront(value){
+    //     newNode = newNode(value);
+    //     newNode.next = this.head;
+    //     this.head = newNode;
+    //     return this;
+    // }
+    // removeFront(){
+
+    // }
+    // frontValue(){
+
+    // }
 }
+var nodeA = new Node(10);
+var nodeB = new Node(17);
+var nodeC = new Node(4);
+var nodeD = new Node(93);
+
+nodeA.next = nodeB;
+nodeB.next = nodeC;
+nodeC.next = nodeD;
+nodeD.next = null;
+
+console.log(nodeA);
+
+// class SLL{
+//     constructor(head){
+
+//     }
