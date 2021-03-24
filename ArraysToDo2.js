65b4c65160c7e5da16cf1d14c882096ae0156b4b
Cameron Smith <theauspiciouscammy@gmail.com>
2021-03-23 23:41:04 -0700

    ArraysToDo2

--- ArraysToDo2.js
+++ ArraysToDo2.js
@@ -6,16 +6,46 @@ function reverse(array){
     }
     return array;
 }
-anArray = [1,2,3,4];
-aNum = 1;
+//anArray = [1,2,3,4];
+//aNum = 1;
 //console.log(reverse(anArray));
 
 function rotate(arr, num){
-    console.log(arr);
-    for(var i = arr.length; i < 0; i--){
-        console.log(arr[i]);
+    var temp = 0;
+    for(var i = arr.length-1; i > 0 ; i--){
+        temp = arr[i];
+        //while? (run as many time as num indicates. as the shift is one at a time.) {
+            arr[i] = arr[i-1];
+            arr[i-1] = temp;
+
+        //}
 
     }
+
     return arr;
 }
-console.log(rotate(anArray, aNum));
+//console.log(rotate(anArray, aNum));
+
+//anArr = [1,6,34,65,23,87,4,6,9,22,33,12,16,18,42,21];
+//var min = 10;
+//var max = 40;
+function filterRange(arr, min, max){
+    for(i = 0; i < arr.length; i++){
+        if(arr[i] > min && arr[i] < max){
+            arr[i] = null;
+        }else if(arr[i] == null){
+            // arr[i] = to the next value that != null
+        }
+    }
+
+    return arr;
+}
+//console.log(filterRange(anArr, min, max));
+anArray = ["a", "b"];
+anotherArray = [1,2];
+function concat(arr1, arr2){
+    newArr = [];
+    for(i = 0)
+    return newArr
+}
+console.log(concat(anArray, anotherArray));
